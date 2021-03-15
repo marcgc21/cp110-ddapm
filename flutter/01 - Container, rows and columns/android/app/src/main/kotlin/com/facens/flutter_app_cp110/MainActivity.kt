8191dc7e3077c2412e1895b46418288c67f06051
Marc Gonzàlez Capdevila <marcgc21@gmail.com>
2021-03-15 14:31:50 -0300

    Reset master branch

--- flutter/01 - Container, rows and columns/android/app/src/main/kotlin/com/facens/flutter_app_cp110/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.facens.flutter_app_cp110
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ flutter/flutter_app_cp110/android/app/src/main/kotlin/com/facens/flutter_app_cp110/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.facens.flutter_app_cp110
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
