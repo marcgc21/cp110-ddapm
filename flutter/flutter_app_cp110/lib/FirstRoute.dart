4c84facabb08a47e35054b6bb565cd98aa0bbf65
Marc Gonzàlez Capdevila <marcgc21@gmail.com>
2021-03-21 11:02:12 -0300

    ListView and ListTile

--- flutter/flutter_app_cp110/lib/FirstRoute.dart
+++ flutter/flutter_app_cp110/lib/FirstRoute.dart
@@ -1,53 +1,136 @@
 import 'package:flutter/material.dart';
 
 class FirstRoute extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('First Route'),
         ),
-        body: Center(
-          child: ListView(
-            padding: const EdgeInsets.all(8),
-            children: <Widget>[
-              ListTile(
-                tile
-                leading:Icon(Icons.access_alarms),
-                title: Container(
-                  color: Colors.red,
-                  width: 30,
-                  child: Text("hoal"),
+        body: ListView(
+          children: [
+           agendaTile(
+             month:"MAR",
+             numberDay: "20",
+             nameDay: "Sábado",
+           ),
+            agendaTile(
+              month:"MAR",
+              numberDay: "21",
+              nameDay: "Domingo",
+            ),
+          ],
+        )
+    );
+  }
+
+}
+
+class agendaTile extends StatelessWidget {
+
+  const agendaTile({
+    this.month,
+    this.numberDay,
+    this.nameDay,
+  });
+
+
+  final String month;
+  final String numberDay;
+  final String nameDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 80,
+          height: 90,
+          color: Colors.red,
+          child: Column(
+            children: [
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      month,
+                      style: TextStyle(
+                      ),
+                    ),
+                  ],
                 ),
-                subtitle: Text("hahahaha"),
-                isThreeLine: true,
-                dense: true,
-                trailing: Icon(Icons.ac_unit_sharp),
               ),
-              ListTile(
-                title: Text("List tile"),
-                subtitle: Text("hahahaha"),
-                isThreeLine: true,
-                dense: true,
-                trailing: Icon(Icons.ac_unit_sharp),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      numberDay,
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              ListTile(
-                title: Text("List tile"),
-                subtitle: Text("hahahaha"),
-                isThreeLine: true,
-                dense: true,
-                trailing: Icon(Icons.ac_unit_sharp),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      nameDay,
+                      style: TextStyle(
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              ListTile(
-                title: Text("List tile"),
-                subtitle: Text("hahahaha"),
-                isThreeLine: true,
-                dense: true,
-                trailing: Icon(Icons.ac_unit_sharp),
-              ),
+
+
             ],
           ),
-        )
+        ),
+        Expanded(
+          child: Container(
+            width: 100,
+            height: 90,
+            color: Colors.blue,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      Text(
+                        "Sabadão na casa do professor de mobile!",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16
+                        ),)
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      Text("Avenida Darci Carvalho")
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      Text("23:00h")
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
