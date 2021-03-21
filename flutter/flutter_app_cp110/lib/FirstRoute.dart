d76e782cd8e88540870e8cc81f2c2bd360970eda
Marc Gonzàlez Capdevila <marcgc21@gmail.com>
2021-03-20 21:40:49 -0300

    Clean directory

--- flutter/flutter_app_cp110/lib/FirstRoute.dart
+++ flutter/flutter_app_cp110/lib/FirstRoute.dart
@@ -8,7 +8,45 @@ class FirstRoute extends StatelessWidget {
           title: Text('First Route'),
         ),
         body: Center(
-          child: Text('First Route'),
+          child: ListView(
+            padding: const EdgeInsets.all(8),
+            children: <Widget>[
+              ListTile(
+                tile
+                leading:Icon(Icons.access_alarms),
+                title: Container(
+                  color: Colors.red,
+                  width: 30,
+                  child: Text("hoal"),
+                ),
+                subtitle: Text("hahahaha"),
+                isThreeLine: true,
+                dense: true,
+                trailing: Icon(Icons.ac_unit_sharp),
+              ),
+              ListTile(
+                title: Text("List tile"),
+                subtitle: Text("hahahaha"),
+                isThreeLine: true,
+                dense: true,
+                trailing: Icon(Icons.ac_unit_sharp),
+              ),
+              ListTile(
+                title: Text("List tile"),
+                subtitle: Text("hahahaha"),
+                isThreeLine: true,
+                dense: true,
+                trailing: Icon(Icons.ac_unit_sharp),
+              ),
+              ListTile(
+                title: Text("List tile"),
+                subtitle: Text("hahahaha"),
+                isThreeLine: true,
+                dense: true,
+                trailing: Icon(Icons.ac_unit_sharp),
+              ),
+            ],
+          ),
         )
     );
   }
