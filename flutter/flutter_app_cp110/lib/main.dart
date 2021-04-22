051c2685fb402bd81bbf53b38239665ff8ec7172
Marc Gonzàlez Capdevila <marcgc21@gmail.com>
2021-04-22 15:00:23 -0300

    Added base firebase structure

--- flutter/flutter_app_cp110/lib/main.dart
+++ flutter/flutter_app_cp110/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_cp110/LoginScreen.dart';
-import 'package:flutter_app_cp110/MyAwesomeApp.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 
